96266a41cd7f4426116ab67d1f32b0d33b7f0bed
Your Name <your@email.com>
2026-09-07 20:20:56 +0000

    Sep 7, 2026, 11:20 PM

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const allowedOrigins = (() => {
 //app.options("*", cors({ origin: allowedOrigins, credentials: true }));
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "",
   credentials: true
 }));
 
