adcf6bef83da9396796478a07828fd8356f2fe25
Your Name <your@email.com>
2026-09-10 23:32:50 +0000

    Sep 11, 2026, 2:32 AM

--- router/users.router.js
+++ router/users.router.js
@@ -6,7 +6,7 @@ const { register, login, getuser, edituser, deleteuser, getallusers, verfication
 const token_verify = require("../modules/authentication/tokenverify");
 const allowedto = require('../modules/authentication/allowedto');
 const role = require('../modules/authentication/role');
-const uploadMiddleware = require("../modules/upload_verification/");
+const uploadMiddleware = require("../modules/upload_verification/avatar");
 const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
