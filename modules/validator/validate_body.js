4b932cd3cfcba6ec8fab810de7524f4da3f6ecd4
Your Name <your@email.com>
2026-09-11 20:51:47 +0000

    Sep 11, 2026, 11:51 PM

--- modules/validator/validate_body.js
+++ modules/validator/validate_body.js
@@ -135,11 +135,5 @@ exports.payvalidator = [
     body('max')
     .notEmpty().withMessage('Item size is required')
     .isNumeric().withMessage('Item size must be a number')
-    .trim(),
-
-    body('callnumber')
-        .notEmpty().withMessage('Call number is required')
-        .isString().withMessage('Call number must be a string')
-        .trim()
-        .isLength({ min: 6, max: 20 }).withMessage('Call number must be between 6 and 20 characters')
+    .trim()
 ]
