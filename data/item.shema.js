2e17106d4e4b30fa73c625c755c6e42aead090b6
Your Name <your@email.com>
2026-09-12 15:46:50 +0000

    Sep 12, 2026, 6:46 PM

--- data/item.shema.js
+++ data/item.shema.js
@@ -26,7 +26,7 @@ const itemSchema = new mongoose.Schema({
     },
     category: {
         type: String,
-        required: true,
+        required: true
     },
     comments: [{
         type: mongoose.Schema.Types.ObjectId,
