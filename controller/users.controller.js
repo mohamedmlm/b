536996ab4186eaa3a37fb203229556ce3a3bc4df
Your Name <your@email.com>
2026-09-12 03:37:26 +0300

    Sep 12, 2026, 3:37 AM

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -95,7 +95,7 @@ const register = asyncwrapper(async (req, res) => {
     verificationCode: hashedVerificationCode,
     role: process.env.MANAGER.split(',').map(e => e.trim().toLowerCase()).includes(email.toLowerCase())? role.MANAGER: role.USER,   
     isEmailVerified: false,
-    avatar: req.file ? req.file.blobUrl : "profile.jp",
+    avatar: req.file ? req.file.blobUrl : ".jp",
     timetodeleteuser: Date.now() + 10 * 60 * 1000,
     notExpiredUntil: Date.now() + 10 * 60 * 1000,
   });
