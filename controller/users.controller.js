f80e9e15aabf1cecb7600101d09fcd37e22fd4ee
Your Name <your@email.com>
2026-09-12 00:26:23 +0000

    Sep 12, 2026, 3:26 AM

--- controller/users.controller.js
+++ controller/users.controller.js
@@ -93,8 +93,7 @@ const register = asyncwrapper(async (req, res) => {
     email,
     password: hashedPassword,
     verificationCode: hashedVerificationCode,
-    role: process.env.MANAGER.split(',').map(e => e.trim().toLowerCase()).includes(email.toLowerCase())
-  ? role.MANAGER
+    role: process.env.MANAGER.split(',').map(e => e.trim().toLowerCase()).includes(email.toLowerCase())? role.MANAGER
   : role.USER,   
     isEmailVerified: false,
     avatar: req.file ? req.file.blobUrl : "profile.jpg",
