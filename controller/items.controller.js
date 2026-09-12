cf9b592158fa5f99d0747ee4dcde2f40612c2f0e
Your Name <your@email.com>
2026-09-12 15:36:08 +0000

    Sep 12, 2026, 6:36 PM

--- controller/items.controller.js
+++ controller/items.controller.js
@@ -54,7 +54,7 @@ const getAllItems = asyncwrapper(async (req, res) => {
     res.status(200).json({ items: sanitizedItems });
 });
 
-const getitems
+const getitemsbycategr
 
 const createItem = asyncwrapper(async (req, res) => {
     const { name, description, min, max, price, category } = req.body;
