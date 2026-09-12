b488b485b7dd1c6fc86eec691b6bb9405d4df4d7
Your Name <your@email.com>
2026-09-12 15:42:06 +0000

    Sep 12, 2026, 6:42 PM

--- controller/items.controller.js
+++ controller/items.controller.js
@@ -37,7 +37,9 @@ const getAllItems = asyncwrapper(async (req, res) => {
     const filter = {
         name: { $regex: safeRegexSearch, $options: "i" },
         price: { $gte: minPrice, $lte: maxPrice },
-        category
+        category: {
+            
+        }
     };
 
     const items = await Items.find(filter, { __v: false })
