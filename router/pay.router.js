d3dcacbccf2cb5c7b229a8a9fcc563abaf75a0aa
Your Name <your@email.com>
2026-09-12 11:16:37 +0000

    Sep 12, 2026, 2:16 PM

--- router/pay.router.js
+++ router/pay.router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const { createPay, deletePay, getAllPays, getisnotpayed, confirmPay, getMyPays } = require('../controller/pay.controller');
+const { createPay, deletePay, getAllPays, getisnotpayed, confirmPay, rejectPay, getMyPays } = require('../controller/pay.controller');
 const tokenverify = require('../modules/authentication/tokenverify');
-const { payvalidator } = require('../modules/validator/validate_body');
+const { payvalidator, rejectPayValidator } = require('../modules/validator/validate_body');
 const validationResult = require('../modules/validator/validate_result');
 const allowedto = require('../modules/authentication/allowedto');
 const role = require('../modules/authentication/role');
@@ -12,5 +12,6 @@ router.get('/my', tokenverify, getMyPays);
 router.get('/getallpays', tokenverify, allowedto(role.MANAGER,role.ADMIN), getAllPays);
 router.get('/getisnotpayed', tokenverify, allowedto(role.MANAGER,role.ADMIN), getisnotpayed);
 router.put('/:payId/confirm', tokenverify, allowedto(role.MANAGER,role.ADMIN), confirmPay);
+router.put('/:payId/reject', tokenverify, allowedto(role.MANAGER,role.ADMIN), rejectPayValidator, validationResult, rejectPay);
 
 module.exports = router;
